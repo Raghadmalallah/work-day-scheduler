8f07fd26629bc368b391f7fd4f0b4173f1975f77
Raghad Malallah <raghadmalallah@gmail.com>
2021-09-30 19:54:55 -0700

    adding var to the collect the moment and hour as current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,34 @@ savebtn.click(function ()) {
   var hourValue = $(this).preventDefault().hourValue();
   localStorage.setItem(hourValue, JSON.stringify(eventEntered));
 };
+
+var currentHour = moment().houre();
 var plannerInput = $(".row input[type=text]");
 
 $(plannerInput).each(function() {
 var inputNumber = parseInt($(this).attr("data-hour"));
+if (inputNumber =< plannerInput) {
+  $(this).addClass('past')
+}
 }
 )
+
+// Given a time block element
+
+// let currentHour = get the current hour
+// let blockHour = get time block hour
+
+// IF currentHour < blockHour
+//    add "future" class to block element
+// ELSE IF currentHour > blockHour
+//    add "past" class to block element
+// ELSE
+//    add "present" class to block element
+
+
+
+
+// currentHour = 9
+// blockHour = 9
+
+// result adds "past" class to time block to make gray
