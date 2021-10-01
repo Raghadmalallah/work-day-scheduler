f8b1b92e776351f93f399db06dd6eae1a4d7bb7a
Raghad Malallah <raghadmalallah@gmail.com>
2021-09-30 19:36:37 -0700

    making a vareable for the row of the planner and save button still have issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,15 @@
-// var currentDate = moment().format(
-//   'dddd'); + " " + moment().format("Do MMM YYYY");
-// var currentTime = moment().format('h:mm;dd a');
+displayEvent() || [];
 
-
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 savebtn.click(function ()) {
   var event = preventDefault;
   var hourValue = $(this).preventDefault().hourValue();
   localStorage.setItem(hourValue, JSON.stringify(eventEntered));
 };
+var plannerInput = $(".row input[type=text]");
 
-// $('.sampleList > li > a').click(function() {
-// 	$(this).parent().toggleClass('sampleClass');
-// 	return false;
-// });
-
-// // moment().format("dddd, MMMM Do YYYY,
-// // var msgInterval = setInterval(function () {
-// //     // If there are no more words left in the message
-// //     if (words[wordCount] === undefined) {
-// //       // Use `clearInterval()` to stop the timer
-// //       clearInterval(msgInterval);
-// //     } else {
-// //       // Display one word of the message
-// //       mainEl.textContent = words[wordCount];
-// //       wordCount++;
-// //     }
-// //   }, 1000);
-// // }
+$(plannerInput).each(function() {
+var inputNumber = parseInt($(this).attr("data-hour"));
+}
+)
