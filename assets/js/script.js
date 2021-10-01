1dbc4ec89d57e6278146b8abaf4190889f2b05e3
Raghad Malallah <raghadmalallah@gmail.com>
2021-09-30 18:50:13 -0700

    working on the save button with changing the class in the html for the nine am

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,36 @@
-var currentDate = moment().format(
-  'dddd'); + " " + moment().format("Do MMM YYYY");
-var currentTime = moment().format('h:mm;dd a');
+// var currentDate = moment().format(
+//   'dddd'); + " " + moment().format("Do MMM YYYY");
+// var currentTime = moment().format('h:mm;dd a');
 
-var nineAm = $("#9am");
+// var nineAm = $("#9am");
 
-var dateInterval = setInterval(function(){
-    if ("#currentDay").momentNow.format("dddd, MMMM Do YYYY") ++ ""
-     + momentNow.format("dddd");
-     $("currentDate").html(currentDate + "" + momentNow.format("hh:mm:ss A"));
-})1000;
+// var dateInterval = setInterval(function(){
+//  var timeNow = moment();
+//  $("currentDay").html(timeNow.format("YYYY MMMM DD") + '' + timeNow.format('dddd').substring(0,3).toUpperCase());
+//  $('#currentDay').html(currentDate + " " +timeNow.format('hh:mm:ss A'));
+// },100);
 
+savebtn.click(function ()) {
+  var event = preventDefault;
+  var hourValue = $(this).preventDefault().hourValue();
+  localStorage.setItem(hourValue, JSON.stringify(eventEntered));
+};
 
-// moment().format("dddd, MMMM Do YYYY,
-// var msgInterval = setInterval(function () {
-//     // If there are no more words left in the message
-//     if (words[wordCount] === undefined) {
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(msgInterval);
-//     } else {
-//       // Display one word of the message
-//       mainEl.textContent = words[wordCount];
-//       wordCount++;
-//     }
-//   }, 1000);
-// }
+// $('.sampleList > li > a').click(function() {
+// 	$(this).parent().toggleClass('sampleClass');
+// 	return false;
+// });
+
+// // moment().format("dddd, MMMM Do YYYY,
+// // var msgInterval = setInterval(function () {
+// //     // If there are no more words left in the message
+// //     if (words[wordCount] === undefined) {
+// //       // Use `clearInterval()` to stop the timer
+// //       clearInterval(msgInterval);
+// //     } else {
+// //       // Display one word of the message
+// //       mainEl.textContent = words[wordCount];
+// //       wordCount++;
+// //     }
+// //   }, 1000);
+// // }
